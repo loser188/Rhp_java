661c56f5eee0b70993998dc4ef77d47505611524
loser188 <1835616829@qq.com>
2022-09-24 17:30:16 +0800

    git 练习20220924

--- src/main/java/com/rhp_demo/git/rhp_demo01.java
+++ src/main/java/com/rhp_demo/git/rhp_demo01.java
@@ -3,6 +3,7 @@ package com.rhp_demo.git;
 public class rhp_demo01 {
     public static void main(String[] args) {
         System.out.println("Hello Git ");
+        System.out.println("测试001");
         //lllllll
     }
 }
